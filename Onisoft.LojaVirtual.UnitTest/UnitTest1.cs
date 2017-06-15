a879598d70b64e92410c5691e7d48828ad086adf
Lucas Maso <lucas.maso@IAPP.CAMBORIU>
2017-06-15 09:48:38 -0300

    Adicionar arquivos de projeto.

---
+++ Onisoft.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Onisoft.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestOnisoft
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Onisoft.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Onisoft.LojaVirtual.Web.MvcApplication" Language="C#" %>
