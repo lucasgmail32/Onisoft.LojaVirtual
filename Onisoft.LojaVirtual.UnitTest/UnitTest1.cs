fd4cb639a2790ccfaa8d35bb6cf74cff4362d2ab
lucas.maso <luckas32@gmail.com>
2017-06-15 13:00:02 -0300

    Testes

--- Onisoft.LojaVirtual.UnitTest/UnitTest1.cs
+++ Onisoft.LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Onisoft.LojaVirtual.Web.Models;
+using Onisoft.LojaVirtual.Web.HtmlHelpers;
 
 namespace Onisoft.LojaVirtual.UnitTest
 {
@@ -7,8 +10,29 @@ namespace Onisoft.LojaVirtual.UnitTest
     public class UnitTestOnisoft
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestarSePaginacaoCorreta()
         {
+            //Arrange
+            HtmlHelper html = null;
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItensPorPagina = 5,
+                ItensTotal = 20
+            };
+
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+
+            // Act
+            MvcHtmlString resultado = html.PageLinks(paginacao, paginaUrl);
+
+            // Assert
+
+            Assert.AreEqual(
+                @"<a class=""btn btn-default"" href=""Pagina1"">1</a>"
+                + @"<a class=""btn btn-default btn-primary selected"" href=""Pagina2"">2</a>"
+                + @"<a class=""btn btn-default"" href=""Pagina3"">3</a>", resultado.ToString()
+                );
         }
     }
 }
